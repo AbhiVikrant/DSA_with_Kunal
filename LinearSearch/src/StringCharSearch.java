b1d1d6f963bc53c6d691fb1e5b7b1ec0d691ec22
AbhiVikrant <vikrantabhishek72@gmail.com>
2025-06-24 08:34:28 +0530

    Search2

--- LinearSearch/src/StringCharSearch.java
+++ LinearSearch/src/StringCharSearch.java
@@ -1,9 +1,15 @@
+import java.util.Arrays;
+
 public class StringCharSearch {
     public static void main(String args[])
     {
         String str="Abhishek";
         char target='c';
         System.out.println(charSearchInString(str,target));
+//        Only toCharArray
+        System.out.println((str.toCharArray()));// Abhishek
+        System.out.println(Arrays.toString(str.toCharArray()));// [A, b, h,i,s,h,e,k]
+        System.out.println(charSearch2(str,'e'));
     }
     static boolean charSearchInString(String str, char target)
     {
@@ -18,4 +24,22 @@ public class StringCharSearch {
         }
         return false;
     }
+//     using enhanced for loop i.e. for each needs collection so first convert string to array
+    static boolean charSearch2(String str, char target)
+    {
+        String str1=str.toLowerCase();
+        if(str1.isEmpty())
+        {
+            return false;
+        }
+        for(char ch: str1.toCharArray())
+        {
+            if(ch==target)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
