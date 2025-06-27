80a02befd5710ebd942a4fbb5af1af664775f7ba
AbhiVikrant <vikrantabhishek72@gmail.com>
2025-06-27 17:43:13 +0530

    Added EasyArray10

--- Codebase/src/EasyArray10.java
+++ Codebase/src/EasyArray10.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 //1832. Check if the Sentence Is Pangram
 //Easy
@@ -23,11 +25,35 @@ import java.util.Arrays;
 //Output: false
 public class EasyArray10 {
     public static void main(String[] args) {
-       checkIfPangram("djujfuuj");
+       System.out.println(checkIfPangram("abcdefghijklmnopqrstuvwxyz"));
+        System.out.println(checkIfPangram2("abhishekkumarvikrant"));
+
     }
     public static boolean checkIfPangram(String sentence) {
-       boolean[] check=new boolean[26];
-       System.out.println(Arrays.toString(check));
-       return false;
+         boolean check[]=new boolean[26];
+         for(int i=0; i<sentence.length(); i++)
+         {
+            char ch=sentence.charAt(i);
+            check[ch-'a']=true;
+         }
+        System.out.println((Arrays.toString(check)));
+        for(int i=0; i< check.length; i++)
+        {
+            if(!check[i])
+                return false;
+        }
+        return true;
+    }
+//    2nd way
+
+    public static boolean checkIfPangram2(String sentence) {
+        char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+        for(char ch : alphabet){
+            if(!sentence.contains(String.valueOf(ch))){
+                return false;
+            }
+        }
+        return true;
     }
 }
