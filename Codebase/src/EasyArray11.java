297cec80d027918a342fca02e422893b477f3580
AbhiVikrant <vikrantabhishek72@gmail.com>
2025-06-27 18:20:00 +0530

    Added easyArray11

---
+++ Codebase/src/EasyArray11.java
@@ -0,0 +1,46 @@
+import java.util.List;
+
+//1773. Count Items Matching a Rule
+//Solved
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//Hint
+//You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+//
+//The ith item is said to match the rule if one of the following is true:
+//
+//ruleKey == "type" and ruleValue == typei.
+//ruleKey == "color" and ruleValue == colori.
+//ruleKey == "name" and ruleValue == namei.
+//Return the number of items that match the given rule.
+//
+//
+//
+//Example 1:
+//
+//Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+//Output: 1
+//Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+public class EasyArray11 {
+    public static void main(String[] args) {
+
+    }
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int count=0;
+        for(int i=0; i<items.size(); i++)
+        {
+
+            if(ruleKey.equals("type")&& ruleValue.equals(items.get(i).get(0)))
+                count++;
+            else if(ruleKey.equals("color")&& ruleValue.equals(items.get(i).get(1)))
+                count++;
+            else if(ruleKey.equals("name")&& ruleValue.equals(items.get(i).get(2)))
+                count++;
+
+
+        }
+        return count;
+    }
+}
